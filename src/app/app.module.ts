4f2cbe00f42a571acc5ad55fa0a099371c973780
root <ami.thakar@volansys.com>
2020-02-06 12:11:59 +0000

    chng..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,42 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { RouterModule,Routes} from '@angular/router';
+import { ProductComponent } from './navbar/product/product.component';
+import { MembersComponent } from './navbar/members/members.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    MembersComponent,
+    ForgotPasswordComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      {
+        path:'signup',
+        component:MembersComponent 
+      },
+      {
+        path:'login',
+        component: ProductComponent 
+      },
+      {
+        path:'forgotpassword',
+        component:ForgotPasswordComponent
+      },
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  
+}
---
+++ src/app/navbar/product/product.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit, ɵɵpureFunction1 } from '@angular/core';
+import {FormGroup , FormControl} from '@angular/forms';
+import {NgModule} from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+  form;
+  ngOnInit() {
+    this.form = new FormGroup({
+      username:new FormControl(""),
+      password:new FormControl(""),
+    });
+  }
+  OnSubmit = function(user){
+    console.log(user);
+  }
+  
+  func1 =  function(){
+
+    let number = "123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    let  x = number[Math.floor(Math.random() * number.length)];
+    for (var i=0;i<4;i++)
+    {
+       x = x + number[Math.floor(Math.random() * number.length)];
+   }
+   document.getElementById("pastedata").innerHTML = x;
+
+           
+  }
+  
+  func2 = function()
+  {
+      
+     let  x= (<HTMLInputElement>document.getElementById("pastedata")).innerHTML;
+      
+     let y =  (<HTMLInputElement>document.getElementById("printdata")).value;
+     
+      if(x === y)
+      {
+        alert("captcha is valid");
+      }
+      else
+      {
+        alert("captcha is invalid");
+      }
+  }
+
+  constructor() { }
+
+}
+  
